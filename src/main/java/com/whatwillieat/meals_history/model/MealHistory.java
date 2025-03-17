f1943f8dcc4983fd3062b2dbd4113c0fa409a31e
Georgi Karchev <georgi.karchev@gmail.com>
2025-03-17 23:22:28 +0200

    Add Entity

---
+++ src/main/java/com/whatwillieat/meals_history/model/MealHistory.java
@@ -0,0 +1,54 @@
+package com.whatwillieat.meals_history.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MealHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID userId; // Who ate the meal
+
+    @Column(nullable = false)
+    private UUID mealId; // The meal they ate
+
+    private Integer rating; // Optional (1-10)
+
+    @Column(nullable = false)
+    private LocalDateTime eatenOn; // When the meal was eaten
+
+    @Column(nullable = false)
+    private LocalDateTime createdOn;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedOn;
+
+    private boolean isDeleted;
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdOn = now;
+        this.updatedOn = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedOn = LocalDateTime.now();
+    }
+}
+
