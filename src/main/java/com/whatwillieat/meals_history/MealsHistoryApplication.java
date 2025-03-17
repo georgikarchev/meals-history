c961f8939c7338e46239b18c2f57ea3a8433bd0c
Georgi Karchev <georgi.karchev@gmail.com>
2025-03-17 23:02:35 +0200

    Initial commit - spring boot app

---
+++ src/main/java/com/whatwillieat/meals_history/MealsHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.whatwillieat.meals_history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealsHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealsHistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whatwillieat/meals_history/MealsHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatwillieat.meals_history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
