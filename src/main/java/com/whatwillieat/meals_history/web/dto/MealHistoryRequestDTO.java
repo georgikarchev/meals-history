f3913bead260ef0ad267c4d03b64ed2649fc2d11
Georgi Karchev <georgi.karchev@gmail.com>
2025-03-17 23:54:34 +0200

    Add dtos

---
+++ src/main/java/com/whatwillieat/meals_history/web/dto/MealHistoryRequestDTO.java
@@ -0,0 +1,31 @@
+package com.whatwillieat.meals_history.web.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MealHistoryRequestDTO {
+    @NotNull
+    private UUID userId;
+
+    @NotNull
+    private UUID mealId;
+
+    @Min(1)
+    @Max(10)
+    private int rating;
+
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime eatenOn;
+}
---
+++ src/main/java/com/whatwillieat/meals_history/web/dto/MealHistoryResponseDTO.java
@@ -0,0 +1,23 @@
+package com.whatwillieat.meals_history.web.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MealHistoryResponseDTO {
+    private UUID id;
+    private UUID userId;
+    private UUID mealId;
+    private int rating;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime eatenOn;
+}
+
